51433ff383830812b82ba7c9d4f09ceb76dbcdca
Lexx-Psi <60262142+Lexx-Psi@users.noreply.github.com>
2021-05-24 16:35:22 +0300

    V2

--- src/main/java/ru/netology/domain/PostSourceInf.java
+++ src/main/java/ru/netology/domain/PostSourceInf.java
@@ -9,7 +9,7 @@ public class PostSourceInformation {
         public String getType() {
             return type;
         }
-
+ 
         public void setType(String type) {
             this.type = type;
         }
